5723ea64521dd01f03f748c005e089406ccc2ca2
User <genik-vlad@mail.ru>
2022-06-09 21:38:03 +0300

    Добавьте файлы проекта.

---
+++ Practic_3_10_1ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Practic_3_10_1ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo btn; // тип переменной для хранения кнопки закрытия консоли.
+            do
+            {
+
+                Console.WriteLine(" Введите число :");
+
+                int a;
+
+                a = int.Parse(Console.ReadLine());
+                if (a % 2 == 0)
+
+
+                {
+                    Console.WriteLine(" Число чётное");
+                    Console.WriteLine(" Для продолжения нажмите enter , для завершения нажмите escape");
+                    
+                }
+                else
+                {
+                    Console.WriteLine(" Число нечётное");
+                    Console.WriteLine(" Для продолжения нажмите enter , для завершения нажмите escape");
+                    
+                }
+                
+
+                btn = Console.ReadKey();
+                Console.Clear();// очищает консоль.
+            }
+            while (btn.Key != ConsoleKey.Escape);
+
+
+        }
+
+    }
+}
